782d013dd1e89580bc1eff9efa1dd5c1ff8d41da
Mayuravarsha <55343260+Mayuravarsha@users.noreply.github.com>
2022-04-27 15:19:00 +0530

    Add files via upload

--- src/Game.java
+++ src/Game.java
@@ -3,12 +3,7 @@ import javax.swing.JFrame;
 
 public class Game {
     public static void main(String[] args) {
-        Board b = new Board();
-        b.setPreferredSize(new Dimension(1030, 1030)); // need to use this instead of setSize
-        b.setLocation(500, 250);
-        b.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        b.instructions();
-        b.pack();
-        b.setVisible(true);
+        MyFrame f = new MyFrame(); // Registration
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
